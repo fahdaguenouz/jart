cd45663615949022854b0dc5a2f35c36ba614c2f
cbenlafk <chakir.benlafkih@gmail.com>
2025-09-27 14:22:33 +0100

    circle

--- Main.java
+++ Main.java
@@ -1,12 +1,14 @@
+
 import geometrical_shapes.*;
 
 public class Main {
+
     public static void main(String[] args) {
         Image image = new Image(1000, 1000);
         Line line = new Line(new Point(60, 200), new Point(900, 500));
-      line.draw(image);
-      Point p = new Point(500, 500);
-p.draw(image);
+        line.draw(image);
+        Point p = new Point(500, 500);
+        p.draw(image);
         Rectangle rectangle = new Rectangle(new Point(50, 50), new Point(300, 200));
         rectangle.draw(image);
         Triangle triangle = new Triangle(new Point(100, 100), new Point(900, 900), new Point(100, 900));
@@ -19,4 +21,3 @@ p.draw(image);
         image.save("image.png");
     }
 }
-
--- geometrical_shapes/Circle.java
+++ geometrical_shapes/Circle.java
@@ -4,6 +4,7 @@ import java.awt.Color;
 import java.util.Random;
 
 public class Circle implements Drawable {
+
     private Point center;
     private int radius;
     private Color color;
@@ -11,23 +12,56 @@ public class Circle implements Drawable {
     public Circle(Point center, int radius) {
         this.center = center;
         this.radius = radius;
-        this.color = Color.BLACK;
+        this.color = randomColor();
     }
 
     public static Circle random(int maxWidth, int maxHeight) {
         Random rand = new Random();
-        int radius = rand.nextInt(50) + 5; 
+        int radius = rand.nextInt(100) + 5;
         return new Circle(Point.random(maxWidth, maxHeight), radius);
-    }
-
-    @Override
-    public void draw(Displayable displayable) {
-      
-        center.draw(displayable);
     }
 
     @Override
     public Color getColor() {
         return color;
+    }
+
+    private Color randomColor() {
+        Random rand = new Random();
+        return new Color(rand.nextInt(256), rand.nextInt(256), rand.nextInt(256));
+    }
+
+    @Override
+    public void draw(Displayable displayable) {
+        drawMidpointCircle(displayable, center.getX(), center.getY(), radius, color);
+    }
+
+    private void drawMidpointCircle(Displayable displayable, int xc, int yc, int r, Color color) {
+        int x = 0;
+        int y = r;
+        int d = 3 - 2 * r;
+
+        while (y >= x) {
+            plotCirclePoints(displayable, xc, yc, x, y, color);
+            x++;
+
+            if (d > 0) {
+                y--;
+                d = d + 4 * (x - y) + 10;
+            } else {
+                d = d + 4 * x + 6;
+            }
+        }
+    }
+
+    private void plotCirclePoints(Displayable displayable, int xc, int yc, int x, int y, Color color) {
+        displayable.display(xc + x, yc + y, color);
+        displayable.display(xc - x, yc + y, color);
+        displayable.display(xc + x, yc - y, color);
+        displayable.display(xc - x, yc - y, color);
+        displayable.display(xc + y, yc + x, color);
+        displayable.display(xc - y, yc + x, color);
+        displayable.display(xc + y, yc - x, color);
+        displayable.display(xc - y, yc - x, color);
     }
 }
